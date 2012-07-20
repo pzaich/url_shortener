d69251e1afe9edc4db2d333f9722b224ef94506a
Paul Zaich <pzaich@gmail.com>
2012-07-20 12:22:44 -0700

    changed the routing to render for create method

--- Gemfile
+++ Gemfile
@@ -21,6 +21,7 @@ group :assets do
 end
 
 gem 'jquery-rails'
+gem 'devise', '2.1.2'
 
 # To use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.0.0'
--- app/controllers/urls_controller.rb
+++ app/controllers/urls_controller.rb
@@ -11,10 +11,12 @@ class UrlsController < ApplicationController
     @url = Url.new(params[:url])
     if @url.save
       @url.vanity ? flash[:messages] = "Share your vanity url, #{root_url}pzz/#{@url.vanity}!" : flash[:messages] = "Your shortened url is #{root_url}pzz/#{@url.id}!"
+      redirect_to new_url_path
     else
-      flash[:error] = "Check to make sure you have a valid Url. Must contain http://. If url valid, try a new vanity url."  
+      flash[:error] = "Check to make sure you have a valid Url. Must contain http://. If url valid, try a new vanity url."
+      render :action => :new  
     end 
-    redirect_to new_url_path
+    
   end
 
   def show
