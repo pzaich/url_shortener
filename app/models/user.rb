6164df105b40860cb291e33197883ca041894a54
Paul Zaich <pzaich@gmail.com>
2012-07-20 14:48:25 -0700

    pg gem

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ActiveRecord::Base
   attr_accessible :email, :username
   validates_presence_of :email, :username
+  
+  validates_format_of :email, :with /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/
+  
+  
 end
