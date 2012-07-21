96fb301e96c1ab7968f45ed3f0f57bf0b4c041fe
Paul Zaich <pzaich@gmail.com>
2012-07-20 17:44:04 -0700

    comment out regex validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   attr_accessible :email, :username
   validates_presence_of :email, :username
   
-  validates :email, :format  => { /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/ } 
+  # validates :email, :format  => { /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/ } 
   validates_length_of :username, :maximum => 12
   validates_presence_of :email, :username
 end
