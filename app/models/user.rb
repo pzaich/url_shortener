cf30fe9b38d12701bc50609b5ca7968fce463696
Paul Zaich <pzaich@gmail.com>
2012-07-20 17:39:38 -0700

    user.rb format validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   attr_accessible :email, :username
   validates_presence_of :email, :username
   
-  # validates_format_of :email, :with /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/
+  validates :email, :format  => { /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/ } 
   validates_length_of :username, :maximum => 12
   validates_presence_of :email, :username
 end
