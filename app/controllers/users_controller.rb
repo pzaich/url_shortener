120d3d7d12930e6fcb3c19c0c8e0ac1de305840d
Paul Zaich <pzaich@gmail.com>
2012-07-22 17:31:40 -0700

    ssl & flash/error messages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,7 @@ class UsersController < ApplicationController
   	@user = User.new(params[:user])
 
   	if @user.save
+  		flash[:messages] = "Thank you for registering."
   		redirect_to @user
   	else
   		render :action => :new
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,6 +20,8 @@ UrlShortener::Application.configure do
   # Generate digests for assets URLs
   config.assets.digest = true
 
+  config.force_ssl = true
+
   # Defaults to Rails.root.join("public/assets")
   # config.assets.manifest = YOUR_PATH
 
