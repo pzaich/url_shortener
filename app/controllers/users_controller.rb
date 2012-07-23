0865b0aeb988ae8002630b7d1e20742c213da309
Paul Zaich <pzaich@gmail.com>
2012-07-22 17:20:53 -0700

    form fix on password field, create controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,14 @@ class UsersController < ApplicationController
   def new
   	@user = User.new
   end
+
+  def create
+  	@user = User.new(params[:user])
+
+  	if @user.save
+  		redirect_to @user
+  	else
+  		render :action => :new
+  	end
+  end
 end
